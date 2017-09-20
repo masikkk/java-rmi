1e7cb6e72a24142b8c8a02cbabd7f63743cb5125
masikkk <masi.1989@163.com>
2017-09-20 14:34:28 +0800

    新建空的客户端项目rmi-client

---
+++ rmi/rmi-client/src/main/java/com/masikkk/rmi/client/App.java
@@ -0,0 +1,13 @@
+package com.masikkk.rmi.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
