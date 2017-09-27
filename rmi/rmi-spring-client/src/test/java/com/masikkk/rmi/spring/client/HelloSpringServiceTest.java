683fc3e53c065997ecbda20525a7e89d70f5e4f5
masikkk <masi.1989@163.com>
2017-09-27 16:41:21 +0800

    客户端代码完成

---
+++ rmi/rmi-spring-client/src/test/java/com/masikkk/rmi/spring/client/HelloSpringServiceTest.java
@@ -0,0 +1,22 @@
+package com.masikkk.rmi.spring.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.masikkk.rmi.spring.server.HelloSpringService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext-client.xml"})
+public class HelloSpringServiceTest {
+	@Autowired
+	HelloSpringService helloSpringService;
+	
+	@Test
+	public void test(){
+		System.out.println(helloSpringService.sayHello("masikkk.spring"));
+		System.out.println(helloSpringService.sum(10, 5));
+	}
+}
